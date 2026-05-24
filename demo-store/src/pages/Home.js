4892fa0ec0bad5f21c51ef283cca2349ae973caf
7hvj <mo1515mo85@gmail.com>
2026-05-24 14:11:53 +0300

    first commit

---
+++ demo-store/src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="container mt-4">
+            <h1>Welcome to Best Store</h1>
+            <p>This is the best store in the world. We have everything you need.</p>
+        </div>
+    )
+}
